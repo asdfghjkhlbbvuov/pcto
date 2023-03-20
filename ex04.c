f5b42f70f6d3db14040202de700dc8e69fc54e32
asdfghjkhlbbvuov <128370145+asdfghjkhlbbvuov@users.noreply.github.com>
2023-03-20 15:31:56 +0100

    Add files via upload

---
+++ ex04.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+int a;
+
+
+printf("inserisci un numero");
+scanf("%d", &a);
+printf("a: %d\n", a);
+
+int a = a+7;
+printf("a+ 7 è uguale a %d", a);
+int a = a-7;
+printf("a-7 è uguale a %d", a);
+int a = a/7;
+printf("a/7 è uguale a %d", a);
+int a = a*7;
+printf("a*7 è uguale a %d", a);
+int a= a%7;
+printf("a%7 è uguale a %d", a);
+
+}
