e5eda38228824424f8d76e82831c33b61d833683
asdfghjkhlbbvuov <128370145+asdfghjkhlbbvuov@users.noreply.github.com>
2023-03-20 16:11:51 +0100

    Add files via upload

---
+++ ex07.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  int x;
+  int y;
+  printf("inserisci un numero");
+    scanf("%d", &x);
+    printf("inserisci un numero");
+scanf("%d", &y);
+  if(x%y==0)  
+  {
+    printf("è un multiplo");
+  }
+}
