bfdf08e28fe009ac4c427e08ff2f83e60ac6a2b4
asdfghjkhlbbvuov <128370145+asdfghjkhlbbvuov@users.noreply.github.com>
2023-03-21 09:39:12 +0100

    Add files via upload

---
+++ ex09.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+
+    printf("inserisci un anno");
+    scanf("%d", &x);
+
+    if((x%4==0 && x%100>0) || x%400==0)
+    {
+        printf("l'anno è bisestile\n");
+    }
+    else
+    printf("non è bisestile");
+
+}
