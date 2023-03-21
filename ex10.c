2fe5515f3714f0f4b4d2603a3200cfbb03b0cf6b
asdfghjkhlbbvuov <128370145+asdfghjkhlbbvuov@users.noreply.github.com>
2023-03-21 09:39:28 +0100

    Add files via upload

---
+++ ex10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    int y;
+    int z;
+
+    printf("inserisci tre numeri");
+    scanf("%d %d %d", &x, &y, &z);
+
+    int somma1 = x+y;
+    int somma2 = y+z;
+    int somma3 = x+z;
+    if(somma1>z && somma2>x && somma3>y)
+    {
+        printf("è un triangolo");
+    }
+    else
+    {    
+        printf("non è un triangolo");
+    }    
+}
